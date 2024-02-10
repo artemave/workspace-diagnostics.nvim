ed04f40ec2ca55a9dab42ef0e4d3e7b655b752cd
artemave <artemave@gmail.com>
2024-02-10 17:02:57 +0100

    Better filetype detection
    Because sometimes `vim.filetype.match` doesn't work (see https://github.com/neovim/neovim/issues/27265)
    
    Fixes #1

--- lua/workspace-diagnostics/init.lua
+++ lua/workspace-diagnostics/init.lua
@@ -29,7 +29,11 @@ end
 
 local function _get_workspace_files()
   if _workspace_files == nil then
-    _workspace_files = WorkspaceDiagnostics.options.workspace_files()
+    _workspace_files = WorkspaceDiagnostics.options.workspace_files() or {}
+
+    _workspace_files = vim.tbl_filter(function(path)
+      return vim.fn.filereadable(path) == 1
+    end, _workspace_files)
 
     _workspace_files = map(_workspace_files, function(_, path)
       return vim.fn.fnamemodify(path, ":p")
@@ -37,6 +41,44 @@ local function _get_workspace_files()
   end
 
   return _workspace_files
+end
+
+local function _detect_filetype(path)
+  local filetype = vim.filetype.match({ filename = path })
+
+  -- vim.filetype.match is not guaranteed to work on filename alone (see https://github.com/neovim/neovim/issues/27265)
+  if not filetype then
+    for _, buf in ipairs(vim.fn.getbufinfo()) do
+      if vim.fn.fnamemodify(buf.name, ":p") == path then
+        return vim.filetype.match({ buf = buf.bufnr })
+      end
+    end
+
+    local bufn = vim.fn.bufadd(path)
+    vim.fn.bufload(bufn)
+
+    filetype = vim.filetype.match({ buf = bufn })
+
+    vim.api.nvim_buf_delete(bufn, { force = true })
+  end
+
+  return filetype
+end
+
+local _detected_filetypes = {}
+
+local function _get_filetype(path)
+  local ext = vim.fn.fnamemodify(path, ":e")
+
+  if _detected_filetypes[ext] then
+    return _detected_filetypes[ext]
+  end
+
+  local filetype = _detect_filetype(path)
+
+  _detected_filetypes[ext] = filetype
+
+  return filetype
 end
 
 --- Populate workspace diagnostics.
@@ -58,11 +100,11 @@ function WorkspaceDiagnostics.populate_workspace_diagnostics(client, bufnr)
   local workspace_files = _get_workspace_files()
 
   for _, path in ipairs(workspace_files) do
+    local filetype = _get_filetype(path)
+
     if path == vim.api.nvim_buf_get_name(bufnr) then
       goto continue
     end
-
-    local filetype = vim.filetype.match({ filename = path })
 
     if not vim.tbl_contains(client.config.filetypes, filetype) then
       goto continue
